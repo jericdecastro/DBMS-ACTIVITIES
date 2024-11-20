9994b5bb3692df7e4b4037bcbbca0d57c57ec984
jericdecastro <jekteezy648@gmail.com>
2024-11-20 16:43:57 +0800

    Add files via upload
    DBMS_Activity_sarisaristore

---
+++ DeCastro_Jeric_S._BSIT2A_Sari-SariStore.sql
@@ -0,0 +1,38 @@
+-- CREATE DATABASE store;
+
+-- CREATE TABLE products (
+-- Product_No int primary key auto_increment,
+-- Product_Name varchar(50) not null,
+-- Price int,
+-- Packaging varchar(50) not null,
+-- Expiration_Date date
+-- );
+
+-- INSERT INTO products (Product_Name, Price, Packaging, Expiration_Date) VALUES
+-- ("Sardines", 27, "Can", "2026-10-16"),
+-- ("Alcohol Drinks", 140, "Glass Bottle", "2030-10-16"),
+-- ("Coffee", 15, "Sachet", "2028-10-16"),
+-- ("Softdrinks", 15, "Bottle", "2027-10-16"),
+-- ("Candy", 5, "Plastic", "2029-10-16"),
+-- ("Toothpaste", 13, "Sachet", "2028-10-16"),
+-- ("Shampoo", 8, "Sachet", "2028-10-16"),
+-- ("Cigarette", 10, "Plastic", "2030-10-16"),
+-- ("Soap", 25, "Plastic", "2032-10-16"),
+-- ("Vinegar", 14, "Sachet", "2029-10-16"),
+-- ("Soy Sauce", 14, "Sachet", "2029-10-16"),
+-- ("Chip", 10, "Plastic", "2030-10-16"),
+-- ("Meet Loaf", 35, "Can", "2027-10-16"),
+-- ("Corned Beef", 35, "Can", "2027-10-16"),
+-- ("Pancit Canton", 20, "Plastic", "2028-10-16"),
+-- ("Egg", 10, "Tray", "2025-10-16"),
+-- ("Bread", 10, "Plastic", "2025-10-16"),
+-- ("Ketchup", 13, "Sachet", "2027-10-16"),
+-- ("Sugar", 25, "Plastic", "2028-10-16"),
+-- ("Cooking Oil", 35, "Glass Bottle", "2028-10-16"),
+-- ("Juice Powder", 22, "Sachet", "2030-10-16"),
+-- ("Crispy Fry", 20, "Sachet", "2030-10-16"),
+-- ("Milk Powder", 15, "Sachet", "2030-10-16"),
+-- ("Magic Sarap", 8, "Sachet", "2030-10-16"),
+-- ("Fabric Conditioner", 12, "Sachet", "2032-10-16")
+
+Select * From products
